cb176ebf90af44512c7a560d774428a2101f5cec
Jens <20886068+JtheB@users.noreply.github.com>
2021-12-17 23:10:19 +0100

    Day 17 the easy part slimified

--- day17.py
+++ day17.py
@@ -5,7 +5,7 @@ def part_one() -> int:
     # get distance from 0 to y_min
     step_size = abs(0 - target_area.y_min - 1)
     tmp_max_height = 0
-    # calculate max point with gauss
+    # calculate max point from gravity
     while step_size >= 0:
         tmp_max_height += step_size
         step_size -= 1
@@ -13,5 +13,10 @@ def part_one() -> int:
     return tmp_max_height
 
 
+def part_one_slim() -> int:
+    return sum([step for step in range(abs(0 - target_area.y_min - 1), -1, -1) if step >= 0])
+
+
 if __name__ == '__main__':
     print(f"Answer 1 for day 17 is {part_one()}")
+    print(f"Answer 1 for day 17 is {part_one_slim()}")
